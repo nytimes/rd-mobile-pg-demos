2ae196c7a63a1a81e5572f53da69be531bd70f0c
sukanya aneja <sukanya.aneja@nytimes.com>
2021-09-13 10:31:46 -0400

    comments

--- demo3/script.js
+++ demo3/script.js
@@ -7,6 +7,7 @@ const canvasParent = document.querySelector('.canvas-parent')
 const scrollElement = document.querySelector('.scroller')
 const loading = document.querySelector('.loading')
 
+// Set up the three.js scene
 const scene = new Scene()
 const camera = new PerspectiveCamera(75, canvasParent.clientWidth/canvasParent.clientHeight, 0.001, 1000)
 const renderer = new WebGLRenderer()
@@ -15,21 +16,28 @@ renderer.outputEncoding = sRGBEncoding
 scene.background = new Color( 0x1d1d1d )
 canvasParent.appendChild(renderer.domElement)
 
+// Initialize loaders
 const loader = new GLTFLoader()
 const dracoLoader = new DRACOLoader()
 dracoLoader.setDecoderPath('https://unpkg.com/three/examples/js/libs/draco/')
 loader.setDRACOLoader(dracoLoader)
 
+// Initialize the CameraRig 
 const rig = new CameraRig(camera, scene)
 let scrollControls, threeDOFControls
 
+// Initialize the controls
 function initializeControls(camera) {
+  // Give the rig the animation clip along with the translation and rotation object names
   rig.setAnimationClip(camera.animations[0], 'translation_null', 'rotation_null')
   rig.setAnimationTime(0)
+
+  // Set up ScrollControls and specify the timing for the transition animations
+  // See https://nytimes.github.io/three-story-controls/docs/three-story-controls.scrollcontrols.html for more
   scrollControls = new ScrollControls(rig, { 
     scrollElement, 
     dampingFactor: 0.5,
-    startOffset: '-50vh',
+    startOffset: '-50vh', 
     endOffset: '-50vh',
     scrollActions: [
       { 
@@ -45,14 +53,16 @@ function initializeControls(camera) {
     ]
   })
   
+  // Function to execute at the specified start and end of the scroll
   function transitionTop(progress) {
     renderer.domElement.style.opacity = progress
   }
-  
   function transitionBottom(progress) {
     renderer.domElement.style.opacity = 1 - progress
   }
 
+  // Set up ThreeDOFControls
+  // See https://nytimes.github.io/three-story-controls/docs/three-story-controls.threedofcontrols.html for more
   threeDOFControls = new ThreeDOFControls(rig, {
     panFactor: Math.PI / 10,
     tiltFactor: Math.PI / 10,
@@ -60,10 +70,12 @@ function initializeControls(camera) {
     pedestalFactor: 0,
   })
 
+  // Enable controls
   scrollControls.enable()
   threeDOFControls.enable()
 }
 
+// Ensure model materials are not lit
 function resetMaterials(model) {
   model.scene.traverse( child => {
     if(child.isMesh) {
@@ -73,6 +85,7 @@ function resetMaterials(model) {
   })
 }
 
+// Wait for both assets to load to hide the loading text and initialize controls
 Promise.all([loader.loadAsync('./assets/0731_ALLEY_1M_6x2K_D_JP100.glb'), loader.loadAsync('./assets/0731_ANIM_CAM_001.glb')])
 .then( assets => {
   const [model, camera] = assets
@@ -83,7 +96,7 @@ Promise.all([loader.loadAsync('./assets/0731_ALLEY_1M_6x2K_D_JP100.glb'), loader
 })
 .catch( e => console.log(e))
 
-
+// Render loop
 function render(t) {
   window.requestAnimationFrame(render)
   if(rig.hasAnimation) {
@@ -93,10 +106,11 @@ function render(t) {
   renderer.render(scene, camera)
 }
 
+// Handle window resize events
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.aspect = canvasParent.clientWidth/canvasParent.clientHeight
   camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setSize(canvasParent.clientWidth, canvasParent.clientHeight)
 })
 
 render()
--- demo4/script.js
+++ demo4/script.js
@@ -7,20 +7,17 @@ import {
   Vector3,
   Quaternion,
   Matrix4,
-  Euler,
-  MathUtils,
-  MeshBasicMaterial
+  Euler
 } from 'three'
 import { Loader3DTiles } from 'three-loader-3dtiles'
-import { CameraRig, FreeMovementControls, StoryPointsControls, CameraHelper } from 'three-story-controls'
+import { CameraRig, StoryPointsControls } from 'three-story-controls'
 import cameraData from './camera-data.js'
 
-const searchParams = new URLSearchParams(window.location.search)
-
+// Set up the three.js scene
 const canvasParent = document.querySelector('.canvas-parent')
 const captionElement = document.querySelector('.caption')
 const scene = new Scene()
-const camera = new PerspectiveCamera(75, canvasParent.clientWidth / canvasParent.clientHeight, 0.001, 1000)
+const camera = new PerspectiveCamera(60, canvasParent.clientWidth / canvasParent.clientHeight, 0.001, 1000)
 const renderer = new WebGLRenderer()
 renderer.setSize(canvasParent.clientWidth, canvasParent.clientHeight)
 renderer.outputEncoding = sRGBEncoding
@@ -28,63 +25,55 @@ canvasParent.appendChild(renderer.domElement)
 
 const clock = new Clock()
 const rig = new CameraRig(camera, scene)
-let tilesRuntime, controls, cameraHelper
+let tilesRuntime
 
-
-if (searchParams.has('helper')) {
-  controls = new FreeMovementControls(rig, {
-    keyboardScaleFactor: 0.05,
-    wheelScaleFactor: 0.01,
-    panDegreeFactor: Math.PI / 3,
-    tiltDegreeFactor: Math.PI / 10,
-    domElement: renderer.domElement
-  })
-  cameraHelper = new CameraHelper(rig, controls, renderer.domElement)
-  document.querySelector('.next').style.display = 'none'
-  document.querySelector('.prev').style.display = 'none'
-} else {
-  const captions = [
-    'Amet id elit et labore cillum elit sit deserunt do reprehenderit occaecat. Enim ipsum sit do sunt aliqua in est.',
-    'Exercitation consectetur est anim excepteur dolor. Sit qui deserunt proident excepteur aliquip sunt esse ullamco ex ipsum Lorem minim consectetur.',
-    'Dolor nostrud amet ad irure qui ea. Fugiat cupidatat anim aliqua duis cupidatat cupidatat nisi. Occaecat eu magna id aliqua.',
-    'Amet cupidatat officia enim eu sunt pariatur tempor consequat culpa ullamco proident. In voluptate ad sit enim ipsum dolore. ',
-    'Aliquip laboris ea non ex incididunt tempor.',
-    'Ad incididunt esse duis occaecat proident nostrud. Minim non est proident consequat excepteur nulla ad esse exercitation amet id.',
-    'Dolor consectetur sint excepteur officia quis do mollit commodo deserunt dolor adipisicing. Minim adipisicing aliqua ipsum voluptate elit amet do do ullamco Lorem Lorem consectetur.',
-    'Amet cillum do laboris nulla. Amet id elit et labore cillum elit sit deserunt do reprehenderit occaecat.',
-    'Amet cupidatat officia enim eu sunt pariatur tempor consequat culpa ullamco proident. In voluptate ad sit enim ipsum dolore. ',
-    'Aliquip laboris ea non ex incididunt tempor.',
-    'Ad incididunt esse duis occaecat proident nostrud. Minim non est proident consequat excepteur nulla ad esse exercitation amet id.',
-    'Dolor consectetur sint excepteur officia quis do mollit commodo deserunt dolor adipisicing. Minim adipisicing aliqua ipsum voluptate elit amet do do ullamco Lorem Lorem consectetur.',
-  ]
-  const pois = cameraData.pois.map((poi, i) => {
-    return {
-      position: new Vector3(...poi.position),
-      quaternion: new Quaternion(...poi.quaternion),
-      duration: poi.duration,
-      ease: 'power1.in',
-    }
-  })
-  controls = new StoryPointsControls(rig, pois)
-  document.querySelector('.next').addEventListener('click', () => controls.nextPOI())
-  document.querySelector('.prev').addEventListener('click', () => controls.prevPOI())
-  controls.addEventListener('update', (e) => {
-    if (e.progress === 0) {
-      captionElement.classList.remove('visible')
-    } else if (e.progress === 1) {
-      captionElement.classList.add('visible')
-      captionElement.innerHTML = `
+// Points of Interest (POIs) captions
+const captions = [
+  'Amet id elit et labore cillum elit sit deserunt do reprehenderit occaecat. Enim ipsum sit do sunt aliqua in est.',
+  'Exercitation consectetur est anim excepteur dolor. Sit qui deserunt proident excepteur aliquip sunt esse ullamco ex ipsum Lorem minim consectetur.',
+  'Dolor nostrud amet ad irure qui ea. Fugiat cupidatat anim aliqua duis cupidatat cupidatat nisi. Occaecat eu magna id aliqua.',
+  'Amet cupidatat officia enim eu sunt pariatur tempor consequat culpa ullamco proident. In voluptate ad sit enim ipsum dolore. ',
+  'Aliquip laboris ea non ex incididunt tempor.',
+  'Ad incididunt esse duis occaecat proident nostrud. Minim non est proident consequat excepteur nulla ad esse exercitation amet id.',
+  'Dolor consectetur sint excepteur officia quis do mollit commodo deserunt dolor adipisicing. Minim adipisicing aliqua ipsum voluptate elit amet do do ullamco Lorem Lorem consectetur.',
+  'Amet cillum do laboris nulla. Amet id elit et labore cillum elit sit deserunt do reprehenderit occaecat.',
+  'Amet cupidatat officia enim eu sunt pariatur tempor consequat culpa ullamco proident. In voluptate ad sit enim ipsum dolore. ',
+  'Aliquip laboris ea non ex incididunt tempor.',
+  'Ad incididunt esse duis occaecat proident nostrud. Minim non est proident consequat excepteur nulla ad esse exercitation amet id.',
+  'Dolor consectetur sint excepteur officia quis do mollit commodo deserunt dolor adipisicing. Minim adipisicing aliqua ipsum voluptate elit amet do do ullamco Lorem Lorem consectetur.',
+]
+// Create POIs with data exported from the CameraHelper tool 
+// (see here for more: https://nytimes.github.io/three-story-controls/#camera-helper)
+// Note: Any method of listing camera position and quaternion will work for StoryPointControls
+const pois = cameraData.pois.map((poi, i) => {
+  return {
+    position: new Vector3(...poi.position),
+    quaternion: new Quaternion(...poi.quaternion),
+    duration: poi.duration,
+    ease: 'power1.in',
+  }
+})
+// Initialize StoryPointControls with poi data
+const controls = new StoryPointsControls(rig, pois)
+document.querySelector('.next').addEventListener('click', () => controls.nextPOI())
+document.querySelector('.prev').addEventListener('click', () => controls.prevPOI())
+// Update captions when the POI changes
+controls.addEventListener('update', (e) => {
+  if (e.progress === 0) {
+    captionElement.classList.remove('visible')
+  } else if (e.progress === 1) {
+    captionElement.classList.add('visible')
+    captionElement.innerHTML = `
       <p><span> ${e.upcomingIndex + 1}/${captions.length} </span> ${captions[e.upcomingIndex]}</p>
       `
-    }
-  })
-  captionElement.addEventListener('click', () => captionElement.classList.toggle('hidden'))
-}
+  }
+})
+captionElement.addEventListener('click', () => captionElement.classList.toggle('hidden'))
 
+// Load the tileset
+// See here for more options: https://github.com/nytimes/three-loader-3dtiles/blob/dev/docs/three-loader-3dtiles.loaderprops.md
 Loader3DTiles.load(
   {
-
-    // https://console.cloud.google.com/storage/browser/_details/rd-big-files/tilesets/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json
     url: 'https://storage.googleapis.com/rd-big-files/tilesets/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json',
     renderer: renderer,
     options: {
@@ -101,25 +90,25 @@ Loader3DTiles.load(
   tilesRuntime = runtime
   scene.add(model)
   controls.enable()
-  if (!cameraHelper) controls.goToPOI(0)
+  controls.goToPOI(0)
 })
 
-
+// Render loop
 function render(t) {
+  window.requestAnimationFrame(render)
   const dt = clock.getDelta()
   if (tilesRuntime) {
     tilesRuntime.update(dt, renderer, camera)
   }
   controls.update(t)
   renderer.render(scene, camera)
-  if (cameraHelper) cameraHelper.update(t)
-  window.requestAnimationFrame(render)
 }
 
+// Handle window resize events
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.aspect = canvasParent.clientWidth / canvasParent.clientHeight
   camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setSize(canvasParent.clientWidth, canvasParent.clientHeight)
 })
 
 render()
